e45379c506846ad670b4155cdf5b57b5448ab81a
kathy2470 <mukisakathy@gmail.com>
2023-11-30 19:35:24 +0300

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    argc = len(argv) - 1
+
+    if argc == 0:
+        print("0 arguments.")
+    else:
+        print("{:d} argument{}:".format(argc, 's' if argc > 1 else ''))
+        for i in range(1, argc + 1):
+            print("{:d}: {}".format(i, argv[i]))
