4ef57a9ae80df4c3a86af86c1ea7a3c37b8c2e81
kathy2470 <mukisakathy@gmail.com>
2024-01-12 11:51:14 +0300

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,7 +8,6 @@ TypeError exception with message a must be an integer or
 b must be an integer
 a and b must be first casted to integers if they are float
 Returns an integer: the addition of a and b
-You are not allowed to import any module
 """
 
 
