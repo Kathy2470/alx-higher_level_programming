e73d70311cf386a26aff8f334fd77c5a8a6d368e
kathy2470 <mukisakathy@gmail.com>
2024-01-14 15:42:37 +0300

    All Python Inheritance files

---
+++ 0x0A-python-inheritance/tests/10-main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+Square = __import__('10-square').Square
+
+s = Square(13)
+
+print(s)
+print(s.area())
+
+
+11-main.py
+
+#!/usr/bin/python3
+Square = __import__('11-square').Square
+
+s = Square(13)
+
+print(s)
+print(s.area())
