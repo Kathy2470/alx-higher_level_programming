8bdb240b9115b209b778f039439804bef1e967d7
kathy2470 <mukisakathy@gmail.com>
2024-01-15 16:05:05 +0300

    5-base_geometry.py

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+"""Defines an empty class BaseGeometry."""
+
 
 class BaseGeometry:
-    """
-    An empty class named BaseGeometry.
-    """
+    """Represent base geometry."""
     pass
