3c96479a07848a6ee5c6ecb86c5a68400b95703d
kathy2470 <mukisakathy@gmail.com>
2024-02-19 20:17:30 +0300

    update

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,28 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
+    for i in range(x):
         try:
-            for i in range(x):
-                try:
-                    if isinstance(my_list[i], int):
-                        print("{:d}".format(my_list[i]), end="")
-                        count += 1
-                except TypeError:
-                    pass
-            print()
-            return count
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            pass
         except IndexError:
-            return count
-
-
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-    nb_print = safe_print_list_integers(my_list, 2)
-    print("nb_print: {:d}".format(nb_print))
-
-    my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-    nb_print = safe_print_list_integers(my_list, len(my_list))
-    print("nb_print: {:d}".format(nb_print))
-
-    nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
-    print("nb_print: {:d}".format(nb_print))
+            raise
+    print()
+    return count
