be8a10a3d056b3e99bf9008b432de1f73d6a42a3
kathy2470 <mukisakathy@gmail.com>
2024-02-19 20:06:46 +0300

    update

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -13,7 +13,6 @@ if __name__ == "__main__":
     if not has_been_print:
         print("{} is not an integer".format(value))
 
-
     value = -89
     has_been_print = safe_print_integer(value)
     if not has_been_print:
