20b2b323dc445f8a92b412c4e62ce5f2c236763e
kathy2470 <mukisakathy@gmail.com>
2024-03-12 02:18:03 +0300

    update

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,14 +2,12 @@
 
 const size = parseInt(process.argv[2]);
 
-if (isNaN(size) || size <= 0) {
-    console.log("Missing size");
+if (isNaN(size)) {
+  console.log('Missing size');
 } else {
+  if (size > 0) {
     for (let i = 0; i < size; i++) {
-        let line = "";
-        for (let j = 0; j < size; j++) {
-            line += "X";
-        }
-        console.log(line);
+      console.log('X'.repeat(size));
     }
+  }
 }
