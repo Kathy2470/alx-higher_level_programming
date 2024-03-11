afc6ab8c4eeba911d5fdb128d478851c18a8b880
kathy2470 <mukisakathy@gmail.com>
2024-03-12 01:56:03 +0300

    update

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 
 const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
-for (let i = 0; i < lines.length; i++) {
-    console.log(lines[i]);
+for (let i = 0; i < myVar.length; i++) {
+    console.log(myVar[i]);
 }
