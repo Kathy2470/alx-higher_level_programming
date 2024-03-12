639f3ba3501214b5fcc9c62dbc17cbd1165df39a
kathy2470 <mukisakathy@gmail.com>
2024-03-12 19:19:49 +0300

    update

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,8 +3,8 @@
 const OldSquare = require('./5-square');
 
 class Square extends OldSquare {
-  charPrint(c = 'X') {
-    let row = c.repeat(this.width);
+  charPrint (c = 'X') {
+    const row = c.repeat(this.width);
     for (let i = 0; i < this.height; i++) {
       console.log(row);
     }
