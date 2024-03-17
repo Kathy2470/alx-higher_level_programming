b60a370a17c6b32ca8aa3e6430614bfe29e6130a
kathy2470 <mukisakathy@gmail.com>
2024-03-18 00:03:36 +0300

    updated

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Script to list all states with a name starting with 'N' (upper N) from the database hbtn_0e_0_usa.
+"""
+
+import MySQLdb
+import sys
+
+def get_states(username, password, database):
+    """
+    Connects to the MySQL server and retrieves states with names starting with 'N' from the specified database.
+
+    Args:
+        username (str): MySQL username.
+        password (str): MySQL password.
+        database (str): Name of the database.
+
+    Returns:
+        list: List of tuples containing state information.
+    """
+    try:
+        db = MySQLdb.connect(host='localhost',
+                             port=3306,
+                             user=username,
+                             passwd=password,
+                             db=database)
+        cursor = db.cursor()
+
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+
+        results = cursor.fetchall()
+
+        cursor.close()
+        db.close()
+
+        return results
+    except MySQLdb.Error as e:
+        print("MySQL Error: ", e)
+        sys.exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    states = get_states(username, password, database)
+
+    for row in states:
+        print(row)
