c414cecb6b3611a28ac3f38402891ddef72efb40
kathy2470 <mukisakathy@gmail.com>
2024-03-18 17:27:28 +0300

    update

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -2,46 +2,8 @@
 """
 Script lists all cities of that state from the database hbtn_0e_4_usa.
 """
-
 import MySQLdb
 import sys
-
-
-def filter_cities(username, password, database, state_name):
-    """
-    Retrieves all cities of a given state from a specified database.
-
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        database (str): Name of the database.
-        state_name (str): Name of the state to filter cities.
-
-    Returns:
-        list: List of tuples containing city information.
-    """
-    try:
-        db = MySQLdb.connect(host='localhost',
-                             port=3306,
-                             user=username,
-                             passwd=password,
-                             db=database)
-        cursor = db.cursor()
-
-        cursor.execute("SELECT cities.name FROM cities \
-                        JOIN states ON cities.state_id = states.id \
-                        WHERE states.name = %s \
-                        ORDER BY cities.id", (state_name,))
-
-        results = cursor.fetchall()
-
-        cursor.close()
-        db.close()
-
-        return results
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-        sys.exit(1)
 
 
 if __name__ == "__main__":
@@ -55,7 +17,27 @@ if __name__ == "__main__":
     database = sys.argv[3]
     state_name = sys.argv[4]
 
-    cities = filter_cities(username, password, database, state_name)
+    try:
+        db = MySQLdb.connect(host='localhost',
+                             port=3306,
+                             user=username,
+                             passwd=password,
+                             db=database)
+        cursor = db.cursor()
 
-    for city in cities:
-        print(city[0])
+        cursor.execute("SELECT cities.name FROM cities \
+                        JOIN states ON cities.state_id = states.id \
+                        WHERE states.name = %s \
+                        ORDER BY cities.id", (state_name,))
+
+        cities = cursor.fetchall()
+
+        cursor.close()
+        db.close()
+
+        for city in cities:
+            print(city[0])
+
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+        sys.exit(1)
