5964b0da8361c5d8aba7736c6b591fda3cd54d18
kathy2470 <mukisakathy@gmail.com>
2024-03-18 14:58:49 +0300

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """
-Script to list all states starting with 'N' from database hbtn_0e_0_usa.
+Script to list all states from the database hbtn_0e_0_usa.
 """
 
 import MySQLdb
 import sys
 
 
-def get_states(username, password, database):
+def filter_states(username, password, database, state_name):
     """
-    Connects to MySQL server and retrieves states with names starting with 'N' from database.
+    Connects to MySQL server and retrieves states based on provided name.
 
     Args:
         username (str): MySQL username.
         password (str): MySQL password.
         database (str): Name of the database.
+        state_name (str): Name of the state to filter.
 
     Returns:
         list: List of tuples containing state information.
@@ -27,8 +28,8 @@ def get_states(username, password, database):
                              db=database)
         cursor = db.cursor()
 
-        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
-
+        cursor.execute("SELECT * FROM states WHERE name LIKE BINARY %s \
+                        ORDER BY id", (state_name,))
         results = cursor.fetchall()
 
         cursor.close()
@@ -41,15 +42,17 @@ def get_states(username, password, database):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>"
+              .format(sys.argv[0]))
         sys.exit(1)
 
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+    state_name = sys.argv[4]
 
-    states = get_states(username, password, database)
+    states = filter_states(username, password, database, state_name)
 
     for row in states:
         print(row)
