ed72fed276f0b4f606894c93e64945eea33b4196
kathy2470 <mukisakathy@gmail.com>
2024-03-18 15:09:15 +0300

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,29 +7,24 @@ import MySQLdb
 import sys
 
 
-def filter_states(username, password, database, state_name):
+def filter_states(username, password, database):
     """
-    Connects to MySQL server and retrieves states based on provided name.
+    Connects to MySQL server and retrieves states starting with 'N'.
 
     Args:
         username (str): MySQL username.
         password (str): MySQL password.
         database (str): Name of the database.
-        state_name (str): Name of the state to filter.
 
     Returns:
         list: List of tuples containing state information.
     """
     try:
-        db = MySQLdb.connect(host='localhost',
-                             port=3306,
-                             user=username,
-                             passwd=password,
-                             db=database)
+        db = MySQLdb.connect(host='localhost', port=3306,
+                             user=username, passwd=password, db=database)
         cursor = db.cursor()
 
-        cursor.execute("SELECT * FROM states WHERE name LIKE BINARY %s \
-                        ORDER BY id", (state_name,))
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
         results = cursor.fetchall()
 
         cursor.close()
@@ -42,17 +37,16 @@ def filter_states(username, password, database, state_name):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>"
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>"
               .format(sys.argv[0]))
         sys.exit(1)
 
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    state_name = sys.argv[4]
 
-    states = filter_states(username, password, database, state_name)
+    states = filter_states(username, password, database)
 
     for row in states:
         print(row)
