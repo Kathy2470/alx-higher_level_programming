c5635425779c60aba295fe517b8a435d14b028f0
kathy2470 <mukisakathy@gmail.com>
2024-03-18 15:21:50 +0300

    update

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
 """
-Script to list all cities of a given state from the database hbtn_0e_4_usa.
+Script lists all cities of that state from the database hbtn_0e_4_usa.
 """
 
 import MySQLdb
 import sys
 
 
-def list_cities_by_state(username, password, database, state_name):
+def filter_cities(username, password, database, state_name):
     """
-    Connects to the MySQL server and lists all cities of the given state.
+    Retrieves all cities of a given state from a specified database.
 
     Args:
         username (str): MySQL username.
         password (str): MySQL password.
         database (str): Name of the database.
-        state_name (str): Name of the state.
+        state_name (str): Name of the state to filter cities.
 
     Returns:
         list: List of tuples containing city information.
     """
     try:
-        # Connect to MySQL server
         db = MySQLdb.connect(host='localhost',
                              port=3306,
                              user=username,
@@ -29,28 +28,26 @@ def list_cities_by_state(username, password, database, state_name):
                              db=database)
         cursor = db.cursor()
 
-        # Execute query to select cities of the given state
-        query = "SELECT cities.id, cities.name FROM cities \
-                 JOIN states ON cities.state_id = states.id \
-                 WHERE states.name = %s ORDER BY cities.id"
-        cursor.execute(query, (state_name,))
+        cursor.execute("SELECT cities.name FROM cities \
+                        JOIN states ON cities.state_id = states.id \
+                        WHERE states.name = %s \
+                        ORDER BY cities.id", (state_name,))
 
-        # Fetch all results
         results = cursor.fetchall()
 
-        # Close cursor and connection
         cursor.close()
         db.close()
 
         return results
     except MySQLdb.Error as e:
-        print("MySQL Error: ", e)
+        print("MySQL Error:", e)
         sys.exit(1)
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        print("Usage: {} <username> <password> <database> <state_name>"
+              .format(sys.argv[0]))
         sys.exit(1)
 
     username = sys.argv[1]
@@ -58,7 +55,7 @@ if __name__ == "__main__":
     database = sys.argv[3]
     state_name = sys.argv[4]
 
-    cities_by_state = list_cities_by_state(username, password, database, state_name)
+    cities = filter_cities(username, password, database, state_name)
 
-    for city in cities_by_state:
-        print(city)
+    for city in cities:
+        print(city[0])
