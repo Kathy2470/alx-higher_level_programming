abbdcc19a6b2b60cc3516df70220903f39f45843
kathy2470 <mukisakathy@gmail.com>
2024-03-18 15:58:32 +0300

    update

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,8 +1,3 @@
-#!/usr/bin/python3
-"""
-Contains class definition of State and an instance Base = declarative_base()
-"""
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -10,12 +5,8 @@ Base = declarative_base()
 
 
 class State(Base):
-    """State class that links to the MySQL table states"""
-    __tablename__ = 'states'
+    """Class representing a state in the database."""
 
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-
-from sqlalchemy import create_engine
-engine = create_engine('mysql://kathy2470:kathylenemukisa@localhost:3306/hbtn_0e_6_usa')
-Base.metadata.create_all(engine)
