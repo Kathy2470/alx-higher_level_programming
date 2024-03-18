aa3aefd4eb6f5a5cfc957f7dbdc3e354052c08be
kathy2470 <mukisakathy@gmail.com>
2024-03-18 17:15:55 +0300

    update

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,8 @@
-from sqlalchemy import Column, Integer, String
+#!/usr/bin/python3
+"""Define State class and create Base instance"""
+
+import sys
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -6,12 +10,14 @@ Base = declarative_base()
 
 class State(Base):
     """Class representing a state in the database."""
-
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
 
 
-connection_string = 'mysql://kathy2470:kathylenemukisa@localhost/hbtn_0e_6_usa'
-engine + create_engine(connection_string)
-Base.metadata.create_all(engine)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
