d763aff48e84a2f99dde930ef3c1403f17cf3b36
kathy2470 <mukisakathy@gmail.com>
2024-03-22 16:56:21 +0300

    update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,58 +1,17 @@
 #!/usr/bin/python3
-"""
-Script to filter states safely using MySQLdb.
-"""
-
+""" lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
 
-def safe_filter_states(username, password, database, state_name):
-    """
-    Filter states safely using MySQLdb.
-
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        database (str): Name of the database.
-        state_name (str): Name of the state to filter.
-
-    Returns:
-        list: List of tuples containing state information.
-    """
-    try:
-        db = MySQLdb.connect(host='localhost',
-                             port=3306,
-                             user=username,
-                             passwd=password,
-                             db=database)
-        cursor = db.cursor()
-
-        cursor.execute("SELECT * FROM states WHERE name=%s ORDER BY id",
-                       (state_name,))
-        results = cursor.fetchall()
-
-        cursor.close()
-        db.close()
-
-        return results
-    except MySQLdb.Error as e:
-        print("MySQL Error: ", e)
-        sys.exit(1)
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>"
-              .format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    states = safe_filter_states(username, password, database, state_name)
-
-    for row in states:
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
+    cur.close()
+    db.close()
