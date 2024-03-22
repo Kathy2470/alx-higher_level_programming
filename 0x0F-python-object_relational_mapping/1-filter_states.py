071f52d436b35c0afc1cbdc0a7e9645efd6e8813
kathy2470 <mukisakathy@gmail.com>
2024-03-22 16:40:20 +0300

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,52 +1,17 @@
 #!/usr/bin/python3
-"""
-Script to list all states from the database hbtn_0e_0_usa.
-"""
-
+""" Lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
 
-def filter_states(username, password, database):
-    """
-    Connects to MySQL server and retrieves states starting with 'N'.
-
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        database (str): Name of the database.
-
-    Returns:
-        list: List of tuples containing state information.
-    """
-    try:
-        db = MySQLdb.connect(host='localhost', port=3306,
-                             user=username, passwd=password, db=database)
-        cursor = db.cursor()
-
-        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
-        results = cursor.fetchall()
-
-        cursor.close()
-        db.close()
-
-        return results
-    except MySQLdb.Error as e:
-        print("MySQL Error: ", e)
-        sys.exit(1)
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>"
-              .format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    states = filter_states(username, password, database)
-
-    for row in states:
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
+    cur.close()
+    db.close()
