e463043af405532b5a26043e28cc055afb2c4e4c
kathy2470 <mukisakathy@gmail.com>
2024-03-29 04:17:12 +0300

    update

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import requests
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    response = requests.get(url)
+    content_type = type(response.text)
+    content = response.text
+
+    print("Body response:")
+    print("\t- type:", content_type)
+    print("\t- content:", content)
