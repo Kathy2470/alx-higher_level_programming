840d5ec9403140264e84b725d3978b5bc3306ce4
kathy2470 <mukisakathy@gmail.com>
2024-04-25 00:54:55 +0300

    update

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Failed to retrieve data. Status code: ${response.statusCode}`);
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  const characters = movie.characters;
+
+  characters.forEach((characterUrl) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      if (response.statusCode !== 200) {
+        console.error(`Failed to retrieve character data. Status code: ${response.statusCode}`);
+        return;
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  });
+});
