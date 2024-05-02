8d2b8528729e16bfd2003007e18a982249927468
kathy2470 <mukisakathy@gmail.com>
2024-05-03 00:55:29 +0300

    update

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Select the <header> element using document.querySelector
+    var header = document.querySelector('header');
+
+    // Update the text color of the <header> element to red
+    header.style.color = '#FF0000';
+});
